e4103b92e869f9e80d9ecfa3d9d5ced33be930f9
高岩ゆりえ <takaiwa@tsumikiinc.com>
2018-04-18 19:13:18 +0900

    スマホのタッチイベント追加と、それに伴う修正

--- src/js/init/index.js
+++ src/js/init/index.js
@@ -21,13 +21,18 @@ export default function() {
     this.slideItems = slideItems;
     this.itemLength = slideItems.length;
     this.index = 0;
+    this.wrap = document.querySelector('.p-zoom-slider__wrap');
     this.Image = document.querySelector('.p-zoom-slider__image');
     this.imageItems = [];
     this.itemText = document.querySelector('.p-zoom-slider__text');
     this.pager = document.querySelector('.p-zoom-slider__pager');
     this.pagerItems = [];
     this.timerId = null;
+    this.isAutoSlide = false;
     this.transition = false;
+    this.isTouchStart = false;
+    this.touchX = 0;
+    this.moveX = 0;
 
     this.init = function() {
       this.initPager();
@@ -61,9 +66,6 @@ export default function() {
     };
 
     this.switchNext = function() {
-      if (this.transition) {
-        return;
-      }
       var index = this.index;
       this.imageItems[index].style.zIndex = '1';
       setTimeout(()=> {
@@ -71,16 +73,37 @@ export default function() {
         this.imageItems[index].style.opacity = '';
         this.imageItems[index].style.transform = '';
         this.imageItems[index].classList.remove('is-visible');
-      }, 800);
-
-
+        this.isAutoSlide = false;
+        this.isTouchStart = false;
+        this.moveX = 0;
+        this.touchX = 0;
+      }, 600);
       this.index ++;
       if (this.index === 3) {
         this.index = 0;
       }
-
       this.setCurrent();
     };
+
+    this.switchPrev = function() {
+      var index = this.index;
+      this.imageItems[index].style.zIndex = '1';
+      setTimeout(()=> {
+        this.imageItems[index].style.transition = '';
+        this.imageItems[index].style.opacity = '';
+        this.imageItems[index].style.transform = '';
+        this.imageItems[index].classList.remove('is-visible');
+        this.isTouchStart = false;
+        this.moveX = 0;
+        this.touchX = 0;
+      }, 600);
+      this.index --;
+      if (this.index === -1) {
+        this.index = 2;
+      }
+      this.setCurrent();
+    };
+
 
     this.setCurrent = function() {
       this.itemText.innerHTML =  this.slideItems[this.index].text;
@@ -100,11 +123,18 @@ export default function() {
 
     this.startSlide = function() {
       this.timerId = setInterval(() => {
+        console.log(this.transition, this.isTouchStart);
+        if (this.transition || this.isTouchStart) {
+          return;
+        }
+        this.isAutoSlide = true;
         this.switchNext();
+        // this.isAutoSlide = false;
       }, 4500);
     };
 
     this.on = function() {
+      //pagerのクリックイベント
       for (var i = 0; i < this.itemLength; i++) {
         const index = i;
         this.pagerItems[index].addEventListener('click', (e)=> {
@@ -125,6 +155,39 @@ export default function() {
           this.setCurrent();
         }, false);
       }
+
+      //スマホのタッチイベント
+      this.wrap.addEventListener('touchstart', (e)=> {
+        if (this.isAutoSlide || this.isTouchStart) {
+          return;
+        }
+        console.log('touchstart');
+        this.isTouchStart = true;
+        this.touchX = event.touches[0].clientX;
+        this.moveX = event.touches[0].clientX;
+        console.log();
+      }, false);
+
+      this.wrap.addEventListener('touchmove', (e)=> {
+        console.log('touchmove');
+        if(this.isTouchStart) {
+          event.preventDefault();
+          this.moveX = event.touches[0].clientX;
+        }
+      }, false);
+      this.wrap.addEventListener('touchend', (e)=> {
+        console.log('touchend');
+        if(this.isTouchStart) {
+          event.preventDefault();
+          if(this.moveX - this.touchX > 0) {
+            this.switchPrev();
+          } else if(this.moveX - this.touchX < 0) {
+            this.switchNext();
+          }
+
+        }
+      }, false);
+
     };
 
   }
